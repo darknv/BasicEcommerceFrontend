7a4fb17b771aaf977ae8c3bf8e80aef7bcd6295e
Darknv <darknvc@gmail.com>
2026-03-18 13:23:48 +0530

    fixed some errors poppin

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,5 @@
-import React, { createContext, useEffect, useState } from 'react'
-
-export const CartContext = createContext();
+import React, { useEffect, useState } from 'react'
+import { CartContext } from './useCart';
 
 
 const CartProvider = ({children}) => {
